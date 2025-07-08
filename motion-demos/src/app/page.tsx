74354349b3fb69a578864003152531edf1a9bc6f
RadhikaGloomcode <148184013+Radhikaa-chauhan@users.noreply.github.com>
2025-07-09 02:58:28 +0530

    Next js learnings

---
+++ motion-demos/src/app/page.tsx
@@ -0,0 +1,28 @@
+
+// FOR REACT
+"use client";
+import {motion} from "motion/react";   
+// import * as motion from"motion/react-client";
+
+
+
+const HomePage = () => {
+return (
+<>
+<div className="flex justify-center items-start mt-[100rem]">
+
+<div className="h-[200vh] w-full flex justify-center">
+
+<div className="bg-white rounded-lg p-6 shadow-lg">
+
+</div>
+
+</div>
+
+</div>
+
+    </>
+  )
+}
+
+export default HomePage
